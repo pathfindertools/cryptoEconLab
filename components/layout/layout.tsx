5d86afb9e182d88443338adbd10b1b07fed8393b
Suzanna Altman <suzannaaltman@gmail.com>
2023-02-23 16:08:48 -0600

    combine tou and pp

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -278,8 +278,7 @@ export const Layout = ({ rawData, data = layoutData, children }) => {
           <div className="bg-black text-primary">
             <div className="max-w-desktop-full mx-auto sm:w-full pt-4 px-4 sm:px-6 pb-6 flex flex-row-reverse justify-between sm:flex-col">
             <ul className="justify-flex-end flex sm:flex-col gap-6 sm:gap-2 sm:w-full sm:mb-6">
-              <li><a className="mg-paragraph-small" href="/terms-of-use">Terms of Use</a></li>
-              <li><a className="mg-paragraph-small" href="/privacy-policy">Privacy Policy</a></li>
+              <li><a className="mg-paragraph-small" href="/terms-of-use-privacy-policy">Terms of Use & Privacy Policy</a></li>
             </ul>
             </div>
 
